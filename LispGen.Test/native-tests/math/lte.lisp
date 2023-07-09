9134af8c893b7aece3a33043e86bc20bb4b9d9da
Eric Lauffenburger <elauffenburger@gmail.com>
2023-07-08 22:36:12 -0700

    fix `math/lte.lisp` test

--- LispGen.Test/native-tests/math/lte.lisp
+++ LispGen.Test/native-tests/math/lte.lisp
@@ -1,3 +1,3 @@
 (do (assert (<= 1 2) t "1 should be less than 2")
     (assert (<= 2 2) t "2 should equal 2")
-    (assert (not (<= 2 1) t "2 should be less than 1")))
+    (assert (not (<= 2 1)) t "2 should be less than 1"))
